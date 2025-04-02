e312ff2f8d85416571d74159fb04688780c2107f
dreaesposito <dreaesposito@hotmail.com>
2025-04-01 22:18:48 -0500

    - make query tabs have a show/hide option
    - add info modal placeholder to explain how to use the query selection section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,39 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
+// import { Link } from "@heroui/link";
+// import { Snippet } from "@heroui/snippet";
+// import { Code } from "@heroui/code";
+// import { button as buttonStyles } from "@heroui/theme";
 
 import { Tab, Tabs } from "@heroui/tabs";
-import { title, querySubtitle as subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+// import { title, querySubtitle as subtitle } from "@/components/primitives";
+// import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
 import Queries from "./queries";
 import { QueryTabs } from "@/types/QueryTabs";
+import { TestTable } from "@/pages/blog.tsx";
+import { useState } from "react";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@heroui/react";
+import { Link } from "@heroui/link";
 
 export default function IndexPage() {
+  const [menuVisible, setMenuVisible] = useState<boolean>(true);
+  const [childData, setChildData] = useState(""); // to display from the child
+  const { isOpen, onOpen, onOpenChange } = useDisclosure(); // for modal
+
+  // a function is passed to the child component (callback function)
+  // so then this funtion is 'called' in the child with the
+  const handleChildData = (first: string, last: string) => {
+    //console.log("Received data from child:", first, first);
+    setChildData(first + " " + last);
+  };
+
   const tabs = [
     {
       id: QueryTabs.skaters,
@@ -28,69 +51,78 @@ export default function IndexPage() {
 
   return (
     <DefaultLayout>
-      <section className="items-center justify-start">
-        <div className="text-center">
-          <Tabs aria-label="Dynamic tabs" items={tabs}>
+      <div className="pb-4">
+        <div className="flex gap-3">
+          <Button
+            className="text-default-500"
+            endContent={
+              menuVisible ? (
+                <i className="bi bi-arrow-up" />
+              ) : (
+                <i className="bi bi-arrow-down" />
+              )
+            }
+            size={"sm"}
+            variant="bordered"
+            onPress={() => setMenuVisible(!menuVisible)}
+          >
+            {`${menuVisible ? "Hide" : "Show"} Queries`}
+          </Button>
+
+          <Link
+            aria-label="Query info"
+            className="text-default-400 text-sm hover:opacity-75 hover:cursor-pointer"
+            onPress={onOpen}
+          >
+            <i className="bi bi-question-circle text-lg"></i>
+          </Link>
+        </div>
+
+        <div className={`${menuVisible ? "" : "hidden"} items-center mt-2 p-2 bg-default/10 shadow-gray-400 rounded-2xl`}>
+          <Tabs
+            className="content-center"
+            isVertical
+            aria-label="Dynamic tabs"
+            items={tabs}
+          >
             {(item) => (
               <Tab key={item.id} title={item.label}>
-                <Queries selectedKey={item.id} />
+                <Queries emitToParent={handleChildData} selectedKey={item.id} />
+                {/*<DisplayTables selectedKey={item.id} />*/}
+                {childData}
               </Tab>
             )}
           </Tabs>
         </div>
-      </section>
+      </div>
 
-      <h1>Testing 123</h1>
+      {/*Table type for each Query type*/}
+      <TestTable />
 
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <span className={title()}>Make&nbsp;</span>
-          <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-          <br />
-          <span className={title()}>
-            websites regardless of your design experience.
-          </span>
-          <div className={subtitle({ class: "mt-4" })}>
-            Beautiful, fast and modern React UI library.
-          </div>
-        </div>
-
-        <div className="flex gap-3">
-          <Link
-            className={buttonStyles({
-              color: "primary",
-              radius: "full",
-              variant: "shadow",
-            })}
-            href="/aege"
-          >
-            Not found page
-          </Link>
-          <Link
-            className={buttonStyles({ variant: "bordered", radius: "full" })}
-            href="/blog"
-          >
-            <GithubIcon size={20} />
-            Default Table
-          </Link>
-          <Link
-            className={buttonStyles({ variant: "bordered", radius: "full" })}
-            href="/docs"
-          >
-            <GithubIcon size={20} />
-            All Players Call
-          </Link>
-        </div>
-
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              Get started by editing{" "}
-              <Code color="primary">pages/index.tsx</Code>
-            </span>
-          </Snippet>
-        </div>
-      </section>
+      {/*info modal popup*/}
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                How to select a query
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam pulvinar risus non risus hendrerit venenatis.
+                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button variant="flat" onPress={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </DefaultLayout>
   );
 }
