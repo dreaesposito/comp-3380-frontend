d5ef63ff321958324d8063faeebc80262dca650f
dreaesposito <dreaesposito@hotmail.com>
2025-04-01 22:16:44 -0500

    - test extra entries for each query type
    - add modal input (right now it's currently triggered on all queries)

--- src/pages/queries.tsx
+++ src/pages/queries.tsx
@@ -1,9 +1,48 @@
 import { querySubtitle, queryTitle } from "@/components/primitives";
 import { QueryTabs } from "@/types/QueryTabs";
 import { Listbox, ListboxItem } from "@heroui/listbox";
-import QueryInput from "./QueryInput";
+// import { Blog } from "@/pages/blog.tsx";
+// import QueryInput from "./QueryInput";
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@heroui/react";
+import { Input } from "@heroui/input";
+import { Button } from "@heroui/button";
+import { useState } from "react";
 
-export default function Queries({ selectedKey }: { selectedKey: QueryTabs }) {
+export default function Queries({
+  selectedKey,
+  emitToParent,
+}: {
+  selectedKey: QueryTabs;
+  emitToParent: (firstName: any, lastName: any) => void;
+}) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  // send the data from the modal to the parent
+  const handleFormSubmit = () => {
+    emitToParent(firstName, lastName);
+    resetForm();
+    onOpenChange();
+  };
+
+  const resetForm = () => {
+    setFirstName("");
+    setLastName("");
+  };
+
+  const handleModalSelection = (queryKey: number, id: number) => {
+    onOpen(); // open the modal
+    console.log(`Query key: ${queryKey}, ID:${id}`);
+  };
+
   const queryComponents: Map<
     QueryTabs,
     { id: number; label: string; description: string }[]
@@ -20,7 +59,17 @@ export default function Queries({ selectedKey }: { selectedKey: QueryTabs }) {
           id: 2,
           label: "Top Point/Goal/Assist Scorers",
           description:
-            "Determine which players had the most points, goals, or assits in a season",
+            "Determine which players had the most points, goals, or assists in a season",
+        },
+        {
+          id: 3,
+          label: "Test Query",
+          description: "123 test description",
+        },
+        {
+          id: 4,
+          label: "Another tes",
+          description: "owengowenowie go owigeo wi owieg ",
         },
       ],
     ],
@@ -56,14 +105,66 @@ export default function Queries({ selectedKey }: { selectedKey: QueryTabs }) {
     <>
       <Listbox className="horizontal-listbox">
         {queryComponents.get(selectedKey)!.map((query) => (
-          <ListboxItem key={query.id}>
+          <ListboxItem
+            key={query.id}
+            className="bg-default/25 rounded-xl h-30"
+            onPress={() => handleModalSelection(selectedKey, query.id)}
+          >
             <span className={queryTitle()}>{query.label}</span>
             <span className={querySubtitle()}>{query.description}</span>
           </ListboxItem>
         ))}
       </Listbox>
 
-      <QueryInput />
+      {/*<QueryInput />*/}
+      <Modal
+        isOpen={isOpen}
+        placement="top-center"
+        onClose={resetForm}
+        onOpenChange={onOpenChange}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Modal Header
+              </ModalHeader>
+              <ModalBody>
+                <Input
+                  // endContent={<h1>endContent</h1>}
+                  isClearable
+                  label="First Name"
+                  placeholder=""
+                  value={firstName}
+                  variant="bordered"
+                  onValueChange={setFirstName}
+                />
+                <Input
+                  // endContent={<h1>endContent</h1>}
+                  isClearable
+                  label="Last Name"
+                  placeholder=""
+                  value={lastName}
+                  variant="bordered"
+                  onValueChange={setLastName}
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="default" variant="flat" onPress={onClose}>
+                  Close
+                </Button>
+                <Button
+                  color="primary"
+                  variant="solid"
+                  onPress={handleFormSubmit}
+                >
+                  Enter
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </>
   );
 }
